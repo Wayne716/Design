62d8ae3ce0ca9d6de2c9f9661e15b3b62b28b7a7
Wayne716 <waynewood@hotmail.com>
2020-06-24 16:17:34 +0800

    Update Builder.cpp

--- Creational Patterns/Builder.cpp
+++ Creational Patterns/Builder.cpp
@@ -1,85 +1,124 @@
-class House {
-public:
-    vector<string> furnitures;
+/*
+ * 基类生成器需要声明实现的接口
+ * 具体生成器实现基类的接口以及返回对象
+ * 主管类负责对生成器的步骤实现
+ * 最后由生成器返回结果
+ */
 
+class Pizza {
+public:
+    vector<string> recipe;
     void display() const {
-        int size = furnitures.size();
-        for (int i=0; i<size; ++i)
-            cout << furnitures[i] << ' ';
+        for (auto& s: recipe)
+            cout << s << ' ';
     }
 };
 
 class Builder {
 public:
-    virtual void Bed() const = 0;
-    virtual void Table() const = 0;
+    virtual ~Builder() {}
+
+    // 具体产品必须要实现的接口，但不必须调用
+    virtual void cheese() const = 0;
+    virtual void sausage() const = 0;
 };
 
-class IKEA : public Builder {
+class PizzaHut : public Builder {
 public:
-    IKEA() { this->reset(); }
-
-    void Bed() const override {
-        this->house->furnitures.emplace_back("Queen Bed");
+    PizzaHut() {
+        this->pizza = make_shared<Pizza>();
     }
 
-    void Table() const override {
-        this->house->furnitures.emplace_back("Work Desk");
+    void cheese() const override {
+        pizza->recipe.emplace_back("Parmesan Cheese.");
+    }
+    void sausage() const override {
+        pizza->recipe.emplace_back("Pepperoni.");
     }
 
-    shared_ptr<House> get() {
-        shared_ptr<House> item = this->house;
-        this->reset();
-        return item;
-    }
-
-    void reset() {
-        this->house = make_shared<House>();
+    // 生成器提供获取结果的方法
+    shared_ptr<Pizza> serve() {
+        shared_ptr<Pizza> ret = this->pizza;
+        this->pizza = make_shared<Pizza>();
+        return ret;
     }
 
 private:
-    shared_ptr<House> house;
+    shared_ptr<Pizza> pizza;
 };
 
-class Director {
+class PapaJohn : public Builder {
 public:
-    void set_builder(shared_ptr<Builder> _builder) {
-        this->builder = std::move(_builder);
+    PapaJohn() {
+        this->pizza = make_shared<Pizza>();
     }
 
-    void build_lite() {
-        this->builder->Bed();
+    void cheese() const override {
+        pizza->recipe.emplace_back("Mozzarella Cheese.");
+    }
+    void sausage() const override {
+        pizza->recipe.emplace_back("Salami.");
     }
 
-    void build_full() {
-        this->builder->Bed();
-        this->builder->Table();
-    };
+    // 生成器提供获取结果的方法
+    shared_ptr<Pizza> serve() {
+        shared_ptr<Pizza> ret = this->pizza;
+        this->pizza = make_shared<Pizza>();
+        return ret;
+    }
 
+private:
+    shared_ptr<Pizza> pizza;
+};
+
+// 主管类只执行生成步骤，不知晓生成的产品
+class Cook {
+public:
+    void set_builder(shared_ptr<Builder> builder) {
+        this->builder = builder;
+    }
+
+    void cheese_pizza() {
+        this->builder->cheese();
+    }
+    void classic_pizza() {
+        this->builder->cheese();
+        this->builder->sausage();
+    }
 
 private:
     shared_ptr<Builder> builder;
 };
 
-void order(Director& director) {
-    shared_ptr<IKEA> builder = make_shared<IKEA>();
-    director.set_builder(builder);
+void order(Cook& c) {
 
-    director.build_lite();
-    shared_ptr<House> lite_house = builder->get();
-    lite_house->display();
+    // 由主管完成生成步骤
+    shared_ptr<PizzaHut> ph = make_shared<PizzaHut>();
+    c.set_builder(ph);
+    c.classic_pizza();
+    shared_ptr<Pizza> food1 = ph->serve();
+    food1->display();
 
     cout << endl;
 
-    director.build_full();
-    shared_ptr<House> full_house = builder->get();
-    full_house->display();
+    // 直接调用生成器
+    shared_ptr<PapaJohn> pj = make_shared<PapaJohn>();
+    pj->cheese();
+    shared_ptr<Pizza> food2 = pj->serve();
+    food2->display();
 }
 
 int main()
 {
-    shared_ptr<Director> director = make_shared<Director>();
-    order(*director);
+    shared_ptr<Cook> c = make_shared<Cook>();
+    order(*c);
 
     return 0;
 }
+
+/*
+
+Parmesan Cheese. Pepperoni.
+Mozzarella Cheese. 
+
+*/
