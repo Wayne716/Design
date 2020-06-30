45153e92490aab62d720556861d69414ab904e96
Wayne716 <waynewood@hotmail.com>
2020-06-30 15:14:23 +0800

    Update Composite.cpp

--- Structural Patterns/Composite.cpp
+++ Structural Patterns/Composite.cpp
@@ -1,91 +1,97 @@
-// 基类声明简单类和复合类的接口
-class Component {
+/*
+ * 组合类声明简单类和复合类的接口
+ * 简单类实现主要的工作
+ */
+
+class Tree {
 protected:
-    Component* parent;
+    shared_ptr<Tree> parent;
 
 public:
-    virtual ~Component() {}
-    void SetParent(Component* _parent) {
-        parent = _parent;
+    virtual ~Tree() = default;
+    void SetParent(shared_ptr<Tree> p) {
+        this->parent = std::move(p);
     }
-    Component* GetParent() const {
-        return this->parent;
+    shared_ptr<Tree> GetParent() const {
+        return parent;
     }
-    virtual void Add(Component* component) {}
-    virtual void Remove(Component* component) {}
-
+    virtual void Add(shared_ptr<Tree> t) {}
+    virtual void Remove(shared_ptr<Tree> t) {}
     virtual bool isComposite() const {
         return false;
     }
-
-    virtual string Operation() const = 0;
+    virtual string display() const = 0;
 };
 
-class Leaf : public Component {
+class Leaf : public Tree {
 public:
-    string Operation() const override {
+    string display() const override {
         return "Leaf";
     }
 };
 
-class Composite: public Component {
+// 复合类将任务委托给简单类
+class Branch : public Tree, public enable_shared_from_this<Tree> {
 protected:
-    list<Component*> children;
+    list<shared_ptr<Tree>> children;
 
 public:
-    void Add(Component* component) override {
-        this->children.push_back(component);
-        component->SetParent(this);
+    void Add(shared_ptr<Tree> t) override {
+        children.push_back(t);
+        t->SetParent(shared_from_this());
     }
-
-    void Remove(Component* component) override {
-        children.remove(component);
-        component->SetParent(nullptr);
+    void Remove(shared_ptr<Tree> t) override {
+        children.remove(t);
+        t->SetParent(nullptr);
     }
-
     bool isComposite() const override {
         return true;
     }
-
-    string Operation() const override {
-        string result;
-        for (const Component* c : children) {
-            if (c == children.back())
-                result += c->Operation();
-            else
-                result += c->Operation() + '+';
+    string display() const override {
+        string res;
+        for (const auto& t : children) {
+            if (t == children.back())
+                res += t->display();
+            else res += t->display() + '+';
         }
-        return '[' + result + ']';
+        return '[' + res + ']';
     }
 };
 
-void Client1(Component* component) {
-    cout << component->Operation();
+void print(const shared_ptr<Tree>& t) {
+    cout << t->display();
 }
 
-void Client2(Component* c1, Component* c2) {
-    if (c1->isComposite())
-        c1->Add(c2);
-    cout << c1->Operation();
-}
 
 int main()
 {
-    Component* simple = new Leaf;
-    Client1(simple);
+    shared_ptr<Tree> leaf = make_shared<Leaf>();
+    print(leaf);
 
     cout << endl;
 
-    Component* tree = new Composite;
-    Component* branch1 = new Composite;
-    Component* leaf1 = new Leaf;
-    Component* leaf2 = new Leaf;
+    shared_ptr<Tree> branch1 = make_shared<Branch>();
+    shared_ptr<Tree> branch2 = make_shared<Branch>();
+    shared_ptr<Tree> leaf1 = make_shared<Leaf>();
+    shared_ptr<Tree> leaf2 = make_shared<Leaf>();
+    shared_ptr<Tree> leaf3 = make_shared<Leaf>();
     branch1->Add(leaf1);
-    branch1->Add(leaf2);
-    tree->Add(branch1);
-    Client1(tree);
+    branch1->Add(branch2);
+    branch2->Add(leaf2);
+    branch2->Add(leaf3);
+    print(branch1);
 
     cout << endl;
 
-    Client2(tree, simple);
+    branch1->Remove(leaf1);
+    branch2->Remove(leaf2);
+    print(branch1);
 }
+
+/*
+
+Leaf
+[Leaf+[Leaf+Leaf]]
+[[Leaf]]
+
+*/
